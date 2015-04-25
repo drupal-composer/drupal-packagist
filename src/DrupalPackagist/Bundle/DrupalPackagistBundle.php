9d1d214d0d5452d1b027f06d725090adbc0d96e4
Florian Weber <florian.weber@ueberbit.de>
2015-04-25 12:51:16 +0200

    Register PackagistWebBundle as parent

--- src/DrupalPackagist/Bundle/DrupalPackagistBundle.php
+++ src/DrupalPackagist/Bundle/DrupalPackagistBundle.php
@@ -6,4 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DrupalPackagistBundle extends Bundle
 {
+    public function getParent()
+    {
+        return 'PackagistWebBundle';
+    }
 }
